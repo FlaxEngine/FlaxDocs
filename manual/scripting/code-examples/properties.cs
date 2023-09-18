2b90a92b73d97e364db409bdb244300080dc8635
Wojtek Figat <wojtek@figat.pl>
2023-09-18 19:18:32 +0200

    Fix example code

--- manual/scripting/code-examples/properties.cs
+++ manual/scripting/code-examples/properties.cs
@@ -2,7 +2,7 @@ using FlaxEngine;
 
 public class MyScript : Script
 {
-	public bool Field1 = 11;
+	public float Field1 = 11;
 	public Color Field2 = Color.Yellow;
 	public DirectionalLight Field3 { get; set; }
 }
